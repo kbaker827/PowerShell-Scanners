bf71517c0d1efb3ac18657a0fe8a2b9ac4ef532f
goofoo <goofoo2@gmail.com>
2026-05-20 08:58:19 -0400

    fix: add DriverName field to Mapped Printers scanner
    Closes #111

--- PowerShell Scanners/Mapped Printers/Mapped Printers.ps1
+++ PowerShell Scanners/Mapped Printers/Mapped Printers.ps1
@@ -1,1 +1,1 @@
-Get-CimInstance Win32_Printer | Where-Object Name -Like "\\*" | Select-Object ShareName, Name, Default
+Get-CimInstance Win32_Printer | Where-Object Name -Like "\\*" | Select-Object ShareName, Name, DriverName, Default
